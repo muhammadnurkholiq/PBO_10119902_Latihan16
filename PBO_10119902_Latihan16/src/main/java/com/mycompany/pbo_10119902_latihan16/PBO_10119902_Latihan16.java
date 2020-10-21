d10c10c7a2a863cdbfa543710653d0b16c8071ed
muhammadnurkholiq <73203850+muhammadnurkholiq@users.noreply.github.com>
2020-10-21 10:48:35 +0700

    Add files via upload
    First Commit

---
+++ PBO_10119902_Latihan16/src/main/java/com/mycompany/pbo_10119902_latihan16/PBO_10119902_Latihan16.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pbo_10119902_latihan16;
+
+/**
+ *
+ * @author Muhammad Nurkholiq
+ */
+public class PBO_10119902_Latihan16 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 0;
+        
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b);
+        System.out.println("Nilai c = " + c+"\n");
+        
+        c = a + b;
+        System.out.println("c = a + b = " + c );
+        
+        c += a;
+        System.out.println("c += a = " + c);
+        
+        c -= a;
+        System.out.println("c -= a = " + c);
+        
+        c *= a;
+        System.out.println("c *= a = " + c);
+        
+        a = 10;
+        c = 15;
+        c /= a;
+        System.out.println("c /= a = " + c);
+        
+        a = 10;
+        c = 15;
+        c %= a;
+        System.out.println("c %= a = " + c);
+        
+        c <<= 2;
+        System.out.println("c <<= 2 = " + c);
+        
+        c >>= 2;
+        System.out.println("c >>= 2 = " + c);
+        
+        c >>= a;
+        System.out.println("c >>= a = " + c);
+        
+        c &= 2;
+        System.out.println("c &= 2 = " + c);
+        
+        c ^= a;
+        System.out.println("c ^= a = " + c);
+        
+        c |= a;
+        System.out.println("c |= a = " + c);
+    }
+}
